c267113e2223c3e42121d1efd5fc0c4f7e75bf0c
shardul-2006 <imshardul16@gmail.com>
2026-08-20 15:27:18 +0530

    Added some functions to the code

--- assignment_6.cpp
+++ assignment_6.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
 using namespace std;
 
+class stack
+{
+    public:
+    char stack_array[50];
+    int top;
+    stack(){
+        top = -1;
+    }
+    void push(char symbol){
+        if(full())
+        cout << "\nStack Overflow";
+        else{
+            top += 1;
+            stack_array[top] = symbol;
+        }
+    }
+    char pop(){
+        if(empty())
+        return '#';
+        else
+        return(stack_array[top--]);
+    }
+    int empty(){
+        if(top==1){
+            return 1;
+        }
+        else
+        return 0;
+    }
+
+    int full(){
+        if(top==49){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+};
+
 int main()
 {
     
